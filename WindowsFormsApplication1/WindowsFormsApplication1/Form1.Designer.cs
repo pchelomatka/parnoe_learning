3e19f60f744b451ca6c6d3ae59ee727b71e92dfd
KirillYaykov <anton97rus@gmail.com>
2018-03-26 20:43:37 +0400

    commit 1

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(298, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 109);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(298, 124);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ТОЛЬКО В ВЕРХНЕМ РЕГИСТРЕ (ПОКА НЕТ ФАЙЛА)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 500);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void IndexOfTextBox(){
+            char a ;            
+            a = textBox1.Text[0];
+            textBox2.Text = a.ToString();
+        }
+
+        public void FromStringToColumn()
+        {
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                textBox2.Text += textBox1.Text[i].ToString() +"\r\n";
+            }
+        }        
+            
+            
+        
+
+        public void test()
+        {          
+            char W = 'W';
+            char R = 'R';
+            char L = 'L';
+            int North = 1;
+            int South = 0;
+            int West = 0;
+            int East = 0;
+
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if ( i > 0 && i< textBox1.Text.Length)
+                {
+                    if(textBox1.Text[i] == W)
+                    {
+                        if(North==1 ^ South==1)
+                        {
+                            textBox2.Text += 3;
+                        }
+
+                        else if (West == 1 ^ East == 1)
+                        {
+                            textBox2.Text += "c";
+                        }
+                    }
+
+                    if (textBox1.Text[i - 1] == W && textBox1.Text[i] == R && textBox1.Text[i + 1] == W)
+                    {
+                        if (North == 1)
+                        {
+                            textBox2.Text += 5;
+                            North = 0;
+                            East = 1;
+                            i = i + 1;
+                        }
+                        else if (South == 1)
+                        {
+                            textBox2.Text += "a";
+                            South = 0;
+                            West = 1;
+                            i = i + 1;
+                        }
+                        else if (West == 1)
+                        {
+                            textBox2.Text += 6;
+                            West = 0;
+                            North = 1;
+                            i = i + 1;
+                        }
+                        else if (East == 1)
+                        {
+                            textBox2.Text += 9;
+                            East = 0;
+                            South = 1;
+                            i = i + 1;
+                        }
+                    }
+
+                    if (textBox1.Text[i - 1] == W && textBox1.Text[i] == L && textBox1.Text[i + 1] == W)
+                    {
+                        if (North == 1)
+                        {
+                            textBox2.Text += 9;
+                            North = 0;
+                            West = 1;
+                            i = i + 1;
+                        }
+                        else if (South == 1)
+                        {
+                            textBox2.Text += 6;
+                            South = 0;
+                            East = 1;
+                            i = i + 1;
+                        }
+                        else if (West == 1)
+                        {
+                            textBox2.Text += 5;
+                            West = 0;
+                            South = 1;
+                            i = i + 1;
+                        }
+                        else if (East == 1)
+                        {
+                            textBox2.Text += "a";
+                            East = 0;
+                            North = 1;
+                            i = i + 1;
+                        }
+                    }
+                }               
+            }                
+            
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //IndexOfTextBox();
+            //FromStringToColumn();
+            test();
+        }
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
